5e6d0b603f9dbfd4941071f5d69689dbf4b6cdcf
agsener <ahmetgokhansener@gmail.com>
2020-05-22 22:16:02 +0300

    Proje oluşturuldu.

---
+++ src/main/java/com/ags/LWTwitterAppServer/LwTwitterAppServerApplication.java
@@ -0,0 +1,14 @@
+package com.ags.LWTwitterAppServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class LwTwitterAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LwTwitterAppServerApplication.class, args);
+	}
+
+}
